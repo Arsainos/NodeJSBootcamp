ed2d1b57d53fe75c287a7b8e65ee1cfa8827c0c1
Arsainosm <arsainos@gmail.com>
2020-03-08 19:49:36 +0300

    routes and 404 page

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -3,27 +3,18 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRouters = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-// this always runs
-app.use('/', (req, res, next) => {
-    next();
-})
+app.use(adminRoutes);
+app.use(shopRouters);
 
-// add middleware
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-}); 
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from node.js!</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 const server = http.createServer(app);
