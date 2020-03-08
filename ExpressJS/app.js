f0bed86daeaf35e2d65c359adb0f6dcf4f028f16
Arsainosm <arsainos@gmail.com>
2020-03-08 16:38:49 +0300

    configure routes in middleware

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -5,12 +5,12 @@ const express = require('express');
 const app = express();
 
 // add middleware
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In middleware!');
-    next(); // allows to continue to the next middleware
+    res.send('<h1>Add Product Page!</h1>');
 }); 
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from node.js!</h1>');
 });
