d2d956d8b15aea36674e5250ed09e0f01df668f7
Arsainosm <arsainos@gmail.com>
2020-03-11 22:41:48 +0300

    add ejs as template engine

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -8,6 +8,9 @@ const shopRouters = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
