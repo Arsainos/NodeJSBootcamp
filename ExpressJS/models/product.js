953cc482dcaf70090ac4f1486bd985f59c32b1fb
Arsainosm <arsainos@gmail.com>
2020-03-11 23:32:55 +0300

    refactoring

--- ExpressJS/models/product.js
+++ ExpressJS/models/product.js
@@ -1,32 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
+const getProductsFromFile = cb => {
+    const p = path.join(path.dirname(process.mainModule.filename), 'data','products.json');
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data','products.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
         });
+        const p = path.join(path.dirname(process.mainModule.filename), 'data','products.json');
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data','products.json');
-        fs.readFile(p, (err, fileContent) => {
-            if(err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 }
